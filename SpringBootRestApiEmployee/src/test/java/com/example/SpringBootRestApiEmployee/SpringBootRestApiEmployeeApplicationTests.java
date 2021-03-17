a783323522b99b5c404622b2c2c9dbf999037f3b
Foltut Daniel <danfoltut@gmail.com>
2021-03-17 16:15:33 +0200

    Added Entity

---
+++ SpringBootRestApiEmployee/src/test/java/com/example/SpringBootRestApiEmployee/SpringBootRestApiEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRestApiEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
